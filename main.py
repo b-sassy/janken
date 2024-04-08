7a537fa57ff25947a1f3e03246a7c4ccb36c5b4b
Your Name <you@example.com>
2024-04-08 18:13:02 +0900

    冗長な処理の解消

--- main.py
+++ main.py
@@ -1,66 +1,45 @@
 import random
+from enum import Enum
+
+
+class JankenHand(Enum):
+    "ジャンケンの手"
+    グー = 0
+    チョキ = 1
+    パー = 2
+
 
 def main():
 
-    janken_dict = {"グー": 0, "チョキ": 1, "パー": 2} #それぞれの手に対応する数字を設定する。
+    janken_dict = {"グー": JankenHand.グー, "チョキ": JankenHand.チョキ, "パー": JankenHand.パー}  # それぞれの手に対応する数字を設定する。
     print("じゃんけんポイ！")
-    while True: #あいこの場合に繰り返す条件を付与（あいこ以外はbreak）
+    while True:  # あいこの場合に繰り返す条件を付与（あいこ以外はbreak）
         print("(グー、チョキ、パーのいずれかを入力してください。)")
-        user_hand = input("あなたの手 -> ")
-        opponent_hand = (random.randint(0, 2)) #0~2の数値をランダムに生成する。
+        user_hand = JankenHand(janken_dict[input("あなたの手 -> ")])
+        opponent_hand = JankenHand(random.randint(0, 2))  # 0~2の数値をランダムに生成する。
         try:
-            if janken_dict[user_hand] == opponent_hand: #作成した辞書のキーに対応するバリューとランダムで生成された値が同じ場合は、あいこ
-                if opponent_hand == 0:
-                    print("相手の手 -> グー")
-                    print("結果：引き分け")
-                    print("=============")
-                    print("あいこでしょ！")
+            if user_hand == opponent_hand:  # 作成した辞書のキーに対応するバリューとランダムで生成された値が同じ場合は、あいこ
+                print(f"相手の手 -> {opponent_hand.name}")
+                print("結果：引き分け")
+                print("=============")
+                print("あいこでしょ！")
 
-                elif opponent_hand == 1:
-                    print("相手の手 -> チョキ")
-                    print("結果：引き分け")
-                    print("=============")
-                    print("あいこでしょ！")
-
-                elif opponent_hand == 2:
-                    print("相手の手 -> パー")
-                    print("結果：引き分け")
-                    print("=============")
-                    print("あいこでしょ！")
-            #下記以降の処理は、それぞれの手に応じた勝ち負けを出力させる。
-            elif janken_dict[user_hand] == 0 and opponent_hand == 1: #グー対チョキ
-                print("相手の手 -> チョキ")
+            # 下記以降の処理は、それぞれの手に応じた勝ち負けを出力させる。
+            elif (user_hand.value - opponent_hand.value) % 3 == 2:  # 自分の手 - 相手の手を3で割ったあまりが2の場合は自分の勝ち
+                print(f"相手の手 -> {opponent_hand.name}")
                 print("結果：あなたの勝ち")
                 break
 
-            elif janken_dict[user_hand] == 0 and opponent_hand == 2: #グー対パー
-                print("相手の手 -> パー")
+            elif (user_hand.value - opponent_hand.value) % 3 == 1:  # 自分の手 - 相手の手を3で割ったあまりが1の場合は自分の負け
+                print(f"相手の手 -> {opponent_hand.name}")
                 print("結果：あなたの負け")
                 break
 
-            elif janken_dict[user_hand] == 1 and opponent_hand == 0: #チョキ対グー
-                print("相手の手 -> グー")
-                print("結果：あなたの負け")
-                break
-
-            elif janken_dict[user_hand] == 1 and opponent_hand == 2: #チョキ対パー
-                print("相手の手 -> パー")
-                print("結果：あなたの勝ち")
-                break
-
-            elif janken_dict[user_hand] == 2 and opponent_hand == 0: #パー対グー
-                print("相手の手 -> グー")
-                print("結果：あなたの勝ち")
-                break
-
-            elif janken_dict[user_hand] == 2 and opponent_hand == 1: #パー対チョキ
-                print("相手の手 -> チョキ")
-                print("結果：あなたの負け")
-                break
-
-        except Exception: #指定した入力（今回の場合は、グー、チョキ、パーのいずれか）以外だとエラーが起きるので、その場合の処理
+        except Exception as e:  # 指定した入力（今回の場合は、グー、チョキ、パーのいずれか）以外だとエラーが起きるので、その場合の処理
+            print(e)
             print("=============")
             print("下記の入力を守ってください。")
+
 
 if __name__ == '__main__':
     main()
